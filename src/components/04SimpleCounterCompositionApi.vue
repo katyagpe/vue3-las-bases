44edb44fb6ae2413d51a6727af851d9dfd49435c
Katya Salas <ksalas@simetrical.com.mx>
2022-06-01 08:06:51 -0500

    avances del curso Primeros pasos en Vue 3 Composition API

---
+++ src/components/04SimpleCounterCompositionApi.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>Simple Counter Composition Api</h3>
+    <h2>{{ counter }}</h2>
+    <h2>{{ counterComputed }}</h2>
+    <button @click="increment">Incrementar</button>
+    <button @click="decrement">Decrementar</button>
+</template>
+
+<script>
+import useCounter from '@/composables/useCounter'
+
+export default {
+    name: "SimpleCounterCompositionApi",
+
+    setup() {
+        const { counter, counterComputed, increment, decrement } = useCounter()
+        return { counter, counterComputed, increment, decrement }
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-list/>
+    <!--<simple-counter-composition-api/>
+    <hr />
+    <watch-counter/>
+    <hr />
+    <reactive-computer/>
+    <hr />
+    <simple-counter/>
+    <hr />
+    <hello-world/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/00HelloWorld.vue'
+import SimpleCounter from '@/components/01SimpleCounter.vue'
+import ReactiveComputer from '@/components/02ReactiveCounter.vue'
+import WatchCounter from '@/components/03WatchCounter.vue'
+import SimpleCounterCompositionApi from '@/components/04SimpleCounterCompositionApi.vue'
+import TodoList from '@/components/05TodoList.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    SimpleCounter,
+    ReactiveComputer,
+    WatchCounter,
+    SimpleCounterCompositionApi,
+    TodoList
   }
 }
 </script>
